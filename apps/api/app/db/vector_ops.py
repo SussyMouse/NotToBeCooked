73b13198aa866213c92d45cb5aace2ffd76b9474
Calvin Khoo <62873716+Calvin105@users.noreply.github.com>
2026-08-25 22:21:26 +0800

    fix: vector_ops.py

--- apps/api/app/db/vector_ops.py
+++ apps/api/app/db/vector_ops.py
@@ -88,10 +88,10 @@ async def _full_text_search(
     ORDER BY rank DESC
     LIMIT 5
     """
-    ts_query = func.replace(func.to_tsquery(
+    ts_query = func.replace(
         "english",
         func.replace(func.plainto_tsquery("english", term).cast(Text), " & ", " | "),
-    )).cast(TSQUERY)
+    ).cast(TSQUERY)
     rank_col = func.ts_rank(Chunk.content_tsv, ts_query).label("rank")
     statement = select(Chunk, File, rank_col).join(File).where(File.id == Chunk.file_id)
     if file_ids:
