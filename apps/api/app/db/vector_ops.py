eea0b87b2b8e8e124b5e51ddc25f9586d8cdbc93
Calvin Khoo <62873716+Calvin105@users.noreply.github.com>
2026-08-31 15:16:25 +0800

    fix(vector_ops): full text search on active chunk only

--- apps/api/app/db/vector_ops.py
+++ apps/api/app/db/vector_ops.py
@@ -68,8 +68,7 @@ async def _vector_similarity_search(
     distance_col = Chunk.embedding.cosine_distance(query_vector).label("distance")
     statement = (
         select(Chunk, File, distance_col)
-        .join(File)
-        .where(File.id == Chunk.file_id)
+        .join(File, col(Chunk.file_id) == col(File.id))
         .join(IngestionRun, col(Chunk.ingestion_run_id) == col(IngestionRun.id))
         .where(col(IngestionRun.is_active).is_(True))
         .order_by(distance_col)
@@ -87,7 +86,9 @@ async def _full_text_search(
 ) -> Sequence[tuple[Chunk, File, float]]:
     """Performs
     SELECT chunk.*, ts_rank(chunk.content_tsv, replace(plainto_tsquery('english', term)::text, ' & ', ' | ')::tsquery) AS rank
-    FROM chunk
+    FROM chunk, ingestion_run
+    WHERE ingestion_run.id == chunk.ingestion_run_id
+    WHERE ingestion_run.is_active
     WHERE chunk.file_id IN file_ids
     WHERE chunk.content_tsv @@ replace(plainto_tsquery('english', term)::text, ' & ', ' | ')::tsquery
     ORDER BY rank DESC
@@ -97,7 +98,12 @@ async def _full_text_search(
         func.plainto_tsquery("english", term).cast(Text), " & ", " | "
     ).cast(TSQUERY)
     rank_col = func.ts_rank(Chunk.content_tsv, ts_query).label("rank")
-    statement = select(Chunk, File, rank_col).join(File).where(File.id == Chunk.file_id)
+    statement = (
+        select(Chunk, File, rank_col)
+        .join(File, col(Chunk.file_id) == col(File.id))
+        .join(IngestionRun, col(Chunk.ingestion_run_id) == col(IngestionRun.id))
+        .where(col(IngestionRun.is_active).is_(True))
+    )
     if file_ids:
         statement = statement.where(col(Chunk.file_id).in_(file_ids))
     statement = (
